ce95be5c42266b679deb0a852b57971e2aa05e3b
mecctro <mecctro@users.noreply.github.com>
2016-09-18 16:23:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,26 @@
-dpkg-reconfigure tzdata /* Set local time to UTC */
-apt-get install git perl-doc -y
+// Set local time to UTC
+//dpkg-reconfigure tzdata
+timedatectl set-timezone Etc/UTC &&
 apt-get install git \
  build-essential libgd-dev \
- libdatetime-perl libmysqlclient-dev zlib1g zlib1g-dev libexpat1-dev libssl-dev \
- mysql-server mysql-client -y
+ libmysqlclient-dev zlib1g zlib1g-dev libexpat1-dev libssl-dev \
+ mysql-server mysql-client -y  &&
 
-git clone https://github.com/SoylentNews/rehash
-cd rehash
-// change repo to - http://archive.apache.org/dist/httpd/
-// line 343 of Makefile missing double quote @ end
-// line 35 of Makefile debian requires nogroup, not nobody
-// change perl to 5.24.0?
-// change perl to 5.20.1
+git clone https://github.com/SoylentNews/rehash &&
+cd rehash &&
 
-adduser rehash
+adduser rehash &&
 
-\wget -O - https://install.perlbrew.pl | bash
+make build-environment &&
+mv /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 &&
+export PATH=/opt/rehash-environment/perl-5.20.0/bin:$PATH &&
+export PATH=/opt/rehash-environment/rehash/bin:$PATH &&
+make build-environment install &&
+make install-dbix-password &&
+install-slashsite -u slash
+
+
+/*\wget -O - https://install.perlbrew.pl | bash
 source ~/perl5/perlbrew/etc/bashrc
 perlbrew install perl-5.20.1
 perlbrew switch perl-5.20.1
@@ -36,4 +41,4 @@ apt-get install Apache2
 cpan -i ExtUtils::Embed
 make install-dbix-password
 
-install-slashsite -u slash
+install-slashsite -u slash*/

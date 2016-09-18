baf1830a7ab31e2c90f6cdf0402c558db4ab7856
mecctro <mecctro@users.noreply.github.com>
2016-09-18 16:35:03 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,17 +6,20 @@ apt-get install git \
  libmysqlclient-dev zlib1g zlib1g-dev libexpat1-dev libssl-dev \
  mysql-server mysql-client -y  &&
 
+// Get rehash
 git clone https://github.com/SoylentNews/rehash &&
 cd rehash &&
 
 adduser rehash &&
 
+// Build rehash
 make build-environment &&
-//cp -R /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 &&
 ln -s /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 &&
 export PATH=/opt/rehash-environment/perl-5.20.0/bin:$PATH &&
 export PATH=/opt/rehash-environment/rehash/bin:$PATH &&
 make build-environment install &&
+
+// Configure rehash
 make install-dbix-password &&
 install-slashsite -u slash
 

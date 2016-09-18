c5040220453ffa7f0ab9bdff0f2576346a88a63c
mecctro <mecctro@users.noreply.github.com>
2016-09-17 21:27:48 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,14 @@ cd rehash
 // change repo to - http://archive.apache.org/dist/httpd/
 // line 343 of Makefile missing double quote @ end
 // line 35 of Makefile debian requires nogroup, not nobody
-// change perl to 5.24.0
+// change perl to 5.24.0?
+// change perl to 5.20.1
 make build-environment install
 mv /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29
+perl -MCPAN -e 'upgrade'
+perl -MCPAN -e "install ModPerl::MM"
+/opt/rehash-environment/httpd-2.2.29
+apt-get install Apache2
+
 cpan -i ExtUtils::Embed
 make install-dbix-password

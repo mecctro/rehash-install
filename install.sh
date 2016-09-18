c6c931abb1d6ae0fb535d11c31bda356ba7470c1
mecctro <mecctro@users.noreply.github.com>
2016-09-17 22:30:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 dpkg-reconfigure tzdata /* Set local time to UTC */
 apt-get install git perl-doc -y
-apt-get install build-essential libgd-dev \
- libmysqlclient-dev zlib1g zlib1g-dev libexpat1-dev libssl-dev -y
-apt-get install mysql-server mysql-client -y
+apt-get install git \
+ build-essential libgd-dev \
+ libmysqlclient-dev zlib1g zlib1g-dev libexpat1-dev libssl-dev \
+ mysql-server mysql-client -y
 
 git clone https://github.com/SoylentNews/rehash
 cd rehash
@@ -11,10 +12,17 @@ cd rehash
 // line 35 of Makefile debian requires nogroup, not nobody
 // change perl to 5.24.0?
 // change perl to 5.20.1
+
+\wget -O - https://install.perlbrew.pl | bash
+perlbrew install perl-5.20.1
+perlbrew switch perl-5.20.1
+
 make build-environment install
 mv /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29
 perl -MCPAN -e 'upgrade'
 perl -MCPAN -e "install ModPerl::MM"
+/opt/rehash-environment/apache-2.2.29/bin/apxs
+/opt/rehash-environment/apache-2.2.29
 /opt/rehash-environment/httpd-2.2.29
 apt-get install Apache2
 

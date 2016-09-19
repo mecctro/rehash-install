94944252239ed43749832b8bbaafe54a54a04629
mecctro <mecctro@users.noreply.github.com>
2016-09-19 02:58:33 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,18 +21,18 @@ mysql -h 127.0.0.1 -e \
 CREATE USER 'rehash'@'%' IDENTIFIED BY 'rehash';
 GRANT ALL ON *.* TO 'rehash'@'%';
 FLUSH PRIVILEGES;" -p &&
-# Make default MySQL instance externally accessable
 #
+# Make default MySQL instance externally accessable
 sed -i 's/bind-address/#bind-address/g' /etc/mysql/my.cnf &&
 service mysql restart &&
-# Build rehash
 #
+# Build rehash
 make build-environment USER=rehash GROUP=rehash -j 8 &&
 ln -s /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 &&
 export PATH=/opt/rehash-environment/perl-5.20.0/bin:$PATH &&
 export PATH=/opt/rehash-environment/rehash/bin:$PATH &&
 make build-environment install -j 8 &&
-# Configure rehash
 #
+# Configure rehash
 make install-dbix-password &&
 install-slashsite -u rehash

c333339f940889c09727c52d2837eeb07c4d3061
mecctro <mecctro@users.noreply.github.com>
2016-09-19 09:22:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -20,6 +20,7 @@ nohup mysqld --skip-grant-tables 2>&1 || true &
 #
 # Add user locally and to DB
 #adduser rehash &&
+echo Enter MySQL root password for rehash user installation:
 mysql -h 127.0.0.1 -e \
  "CREATE DATABASE rehash;
 CREATE USER 'rehash'@'%' IDENTIFIED BY 'rehash';
@@ -34,6 +35,7 @@ service mysql restart &&
 cd rehash &&
 make build-environment USER=rehash GROUP=rehash -j 8 || true &&
 make build-environment USER=rehash GROUP=rehash -j 8 || true &&
+# symlink addresses problem with change in folder name from repo, and apxs defaults
 ln -s /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 || true &&
 export PATH=/opt/rehash-environment/perl-5.20.0/bin:$PATH &&
 make install-dbix-password &&
@@ -41,4 +43,11 @@ make build-environment install -j 8 || true &&
 export PATH=/opt/rehash-environment/rehash/bin:$PATH &&
 #
 # Configure rehash
-install-slashsite -u rehash
+install-slashsite -u rehash &&
+
+# Setup and start apache / rehash
+export PATH=/opt/rehash-environment/apache-2.2.29/bin:$PATH &&
+export PATH=/opt/rehash-environment/rehash/bin:$PATH &&
+sed -i 's/rehash:80/*:80/g' /opt/rehash-environment/rehash/site/rehash/rehash.conf &&
+apachectl -k start &&
+/etc/init.d/slash start

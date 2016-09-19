3302c747a0ca056f2a66de81dcbd2dc0bdd604b1
mecctro <mecctro@users.noreply.github.com>
2016-09-19 03:54:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -30,15 +30,11 @@ sed -i 's/bind-address/#bind-address/g' /etc/mysql/my.cnf &&
 ##service mysql restart &&
 #
 # Build rehash
-make build-environment USER=rehash GROUP=rehash -j 8 || true &
-wait
-make build-environment USER=rehash GROUP=rehash -j 8 || true &
-wait
+make build-environment USER=rehash GROUP=rehash || true &&
 ln -s /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 &&
 export PATH=/opt/rehash-environment/perl-5.20.0/bin:$PATH &&
 export PATH=/opt/rehash-environment/rehash/bin:$PATH &&
-make build-environment install -j 8 || true &
-wait
+make build-environment install || true &&
 #
 # Configure rehash
 make install-dbix-password &&

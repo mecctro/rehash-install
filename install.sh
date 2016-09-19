d87c4ebae95fb686e43c8a8a6ac2d7afdfa58028
mecctro <mecctro@users.noreply.github.com>
2016-09-19 02:30:07 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,16 +10,30 @@ apt-get install git \
 git clone https://github.com/SoylentNews/rehash &&
 cd rehash &&
 
-// Configure user
-adduser slash &&
+// Configure MySQL
+service mysql stop &&
+mysqld --skip-grant-tables &
+
+// Add user locally and to DB
+adduser rehash &&
+
+mysql -h 127.0.0.1 -e \
+ "CREATE DATABASE rehash;
+CREATE USER 'rehash'@'%' IDENTIFIED BY 'rehash';
+UPDATE mysql.user SET Grant_priv='Y', Super_priv='Y' WHERE User='rehash';
+FLUSH PRIVILEGES;" -p &&
+
+// Make default MySQL instance externally accessable
+sed -i 's/bind-address/#bind-address/g' /etc/mysql/my.cnf &&
+service mysql restart &&
 
 // Build rehash
-make build-environment &&
+make build-environment USER=rehash GROUP=rehash -j 8 &&
 ln -s /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 &&
 export PATH=/opt/rehash-environment/perl-5.20.0/bin:$PATH &&
 export PATH=/opt/rehash-environment/rehash/bin:$PATH &&
-make build-environment install &&
+make build-environment install -j 8 &&
 
 // Configure rehash
 make install-dbix-password &&
-install-slashsite -u slash
+install-slashsite -u rehash

6f379f5f1903b2b9605a40fb71a07fdc5428dc65
mecctro <mecctro@users.noreply.github.com>
2016-09-20 22:42:51 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -69,7 +69,7 @@ service mysql restart &&
 # Build rehash
 #
 cd ${realpath}rehash &&
-make build-environment USER=$user GROUP=$user -j $jobs || true &&
+make build-environment USER=$user GROUP=$user || true &&
 # symlink addresses problem with change in folder name from repo, and apxs defaults
 ln -s /opt/rehash-environment/apache-2.2.29 /opt/rehash-environment/httpd-2.2.29 || true &&
 export PATH=/opt/rehash-environment/perl-5.20.0/bin:$PATH &&
